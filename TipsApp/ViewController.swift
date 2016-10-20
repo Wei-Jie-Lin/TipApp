b88e38ab4367e7a09f682b90785a58da705404a1
家人一暐潔 <Wei-Jie@linweijiede-macbook-pro.local>
2016-10-21 02:08:59 +0800

    完成

--- TipsApp/ViewController.swift
+++ TipsApp/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountTF: UITextField!
+
+    @IBOutlet weak var tipTF: UITextField!
+    
+    @IBOutlet weak var showTV: UITextView!
+    
+    
+    @IBAction func calcBT(_ sender: AnyObject) {
+        
+        if amountTF.text == "" || tipTF.text == "" {
+            showTV.text = " No Data !!! "
+        } else {
+            let tip = Double(amountTF.text!)! * Double(tipTF.text!)!
+            showTV.text = "Your tip : \(String(tip))"
+        }
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
